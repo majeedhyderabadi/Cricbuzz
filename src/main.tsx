32f15f90cd62d5a66ce92ebf7398933e41beb4f8
msoftengg <notifymajeed@gmail.com>
2026-07-01 13:13:54 +0530

    file extension has been chnaged to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import "./styles/global.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-import App from './App.jsx'
+import App from './App.tsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
