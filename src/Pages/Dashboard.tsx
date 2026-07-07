58bac343df7bfa1a276f3a142fba8231b21d8ad4
ashishpurohitNV <ashish.purohit@newvision-software.com>
2026-07-07 13:46:08 +0530

    Live stat UI design completed

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -6,6 +6,7 @@ import LiveCommentary from "../components/Commentary/LiveCommentary";
 import SearchBar from "../components/Search/SearchBar";
 import {useState} from "react";
 import RecentEntries from "../components/RecentEntries/RecentEntries";
+import LiveStatDetails from "../components/LiveStatDetails/LiveStatDetails";
 
 
 function Dashboard() {
@@ -31,6 +32,28 @@ function Dashboard() {
             <div className="Commentry_Performers">
                <LiveCommentary />
                <TopPerformers />
+            </div>
+
+            <div className="live-stat-details-container">      
+                <LiveStatDetails
+                    matchStatus="LIVE"
+                    venue="1st Innings - Over 11.2"
+                    
+                    homeTeam="Mumbai"
+                    homeScore={106}
+                    homeSubText="2 WKTS"
+                    isHomeActive={true}
+                    
+                    awayTeam="Kolkata"
+                    awayScore={0}
+                    awaySubText="0 WKTS"
+                    
+                    stats={[
+                        { label: "RUN RATE", homeValue: 8.4, awayValue: 7.1, maxValue: 15 },
+                        { label: "BOUNDARIES", homeValue: 14, awayValue: 11, maxValue: 30 },
+                        { label: "DOT BALLS", homeValue: 38, awayValue: 47, maxValue: 60 }
+                    ]}
+                    />
             </div>
         
             <RecentEntries />
