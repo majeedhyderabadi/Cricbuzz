19cdfc1344fe3a8a3c0d328cf55a3bc7fc4e1d6e
msoftengg <notifymajeed@gmail.com>
2026-07-20 18:22:21 +0530

    refactor

--- src/Pages/Admin/TeamAndPlayersTab/TeamsPlayers.tsx
+++ src/Pages/Admin/TeamAndPlayersTab/TeamsPlayers.tsx
@@ -68,21 +68,8 @@ const loadTeams = async () => {
             </section>
         </main>
 
+  );
 
-
-
-function TeamsPlayers() {
-    return (
-        <div className="teams-page">
-            <div className="teams-left">
-                <CreateTeam team={null} isDialog={false} />
-                <AddPlayer player={null} isDialog={false} />
-            </div>
-            <div className="teams-right">
-                <TeamRoster />
-            </div>
-        </div>
-    );
-}
+};
 
 export default TeamsPlayers;
